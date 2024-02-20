c6aa5b1eb67e5107bda97309ec306f234fd702d2
JoseRobson2000 <129907542+JoseRobson2000@users.noreply.github.com>
2024-02-19 21:51:22 -0300

    Primeiro componente simples.

---
+++ app_01/src/pages/index.js
@@ -0,0 +1,12 @@
+import SubTitulo from "@/components/SubTitulo";
+import Titulo from "@/components/Titulo";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Titulo></Titulo>
+      <SubTitulo texto = 'Estou aqui'></SubTitulo>
+    </div>
+  );
+}
